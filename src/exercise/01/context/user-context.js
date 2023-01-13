707ae515f8602caef0449b3878201add53e7b1cf
Jonas Büchel <jonas@frontify.com>
2023-01-13 15:12:31 +0100

    Exercise 1: Restructure code into multiple files

---
+++ src/exercise/01/context/user-context.js
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import {useAuth} from '../../../auth-context'
+
+const UserContext = React.createContext()
+UserContext.displayName = 'UserContext'
+
+function userReducer(state, action) {
+  switch (action.type) {
+    case 'start update': {
+      return {
+        ...state,
+        user: {...state.user, ...action.updates},
+        status: 'pending',
+        storedUser: state.user,
+      }
+    }
+    case 'finish update': {
+      return {
+        ...state,
+        user: action.updatedUser,
+        status: 'resolved',
+        storedUser: null,
+        error: null,
+      }
+    }
+    case 'fail update': {
+      return {
+        ...state,
+        status: 'rejected',
+        error: action.error,
+        user: state.storedUser,
+        storedUser: null,
+      }
+    }
+    case 'reset': {
+      return {
+        ...state,
+        status: null,
+        error: null,
+      }
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+}
+
+function UserProvider({children}) {
+  const {user} = useAuth()
+  const [state, dispatch] = React.useReducer(userReducer, {
+    status: null,
+    error: null,
+    storedUser: user,
+    user,
+  })
+  const value = [state, dispatch]
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
+
+function useUser() {
+  const context = React.useContext(UserContext)
+  if (context === undefined) {
+    throw new Error(`useUser must be used within a UserProvider`)
+  }
+  return context
+}
+
+// 🐨 add a function here called `updateUser`
+// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
+// this function. It should accept: dispatch, user, and updates
+
+export {UserProvider, useUser}
---
+++ src/exercise/01/screens/user-profile.js
@@ -0,0 +1,105 @@
+// src/screens/user-profile.js
+// import {UserProvider, useUser} from './context/user-context'
+import {useUser} from '../context/user-context'
+import * as React from 'react'
+import {dequal} from 'dequal'
+import * as userClient from '../../../user-client'
+
+const UserSettings = () => {
+  const [{user, status, error}, userDispatch] = useUser()
+
+  const isPending = status === 'pending'
+  const isRejected = status === 'rejected'
+
+  const [formState, setFormState] = React.useState(user)
+
+  const isChanged = !dequal(user, formState)
+
+  function handleChange(e) {
+    setFormState({...formState, [e.target.name]: e.target.value})
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    // 🐨 move the following logic to the `updateUser` function you create above
+    userDispatch({type: 'start update', updates: formState})
+    userClient.updateUser(user, formState).then(
+      updatedUser => userDispatch({type: 'finish update', updatedUser}),
+      error => userDispatch({type: 'fail update', error}),
+    )
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div style={{marginBottom: 12}}>
+        <label style={{display: 'block'}} htmlFor="username">
+          Username
+        </label>
+        <input
+          id="username"
+          name="username"
+          disabled
+          readOnly
+          value={formState.username}
+          style={{width: '100%'}}
+        />
+      </div>
+      <div style={{marginBottom: 12}}>
+        <label style={{display: 'block'}} htmlFor="tagline">
+          Tagline
+        </label>
+        <input
+          id="tagline"
+          name="tagline"
+          value={formState.tagline}
+          onChange={handleChange}
+          style={{width: '100%'}}
+        />
+      </div>
+      <div style={{marginBottom: 12}}>
+        <label style={{display: 'block'}} htmlFor="bio">
+          Biography
+        </label>
+        <textarea
+          id="bio"
+          name="bio"
+          value={formState.bio}
+          onChange={handleChange}
+          style={{width: '100%'}}
+        />
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setFormState(user)
+            userDispatch({type: 'reset'})
+          }}
+          disabled={!isChanged || isPending}
+        >
+          Reset
+        </button>
+        <button
+          type="submit"
+          disabled={(!isChanged && !isRejected) || isPending}
+        >
+          {isPending
+            ? '...'
+            : isRejected
+            ? '✖ Try again'
+            : isChanged
+            ? 'Submit'
+            : '✔'}
+        </button>
+        {isRejected ? <pre style={{color: 'red'}}>{error.message}</pre> : null}
+      </div>
+    </form>
+  )
+}
+
+const UserDataDisplay = () => {
+  const [{user}] = useUser()
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
+}
+
+export {UserSettings, UserDataDisplay}
